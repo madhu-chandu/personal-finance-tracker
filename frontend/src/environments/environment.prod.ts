263b5eddc995e30f26aa39eb24212ceac40b9f44
Chandana <159031538+madhu-chandu@users.noreply.github.com>
2026-07-19 18:23:52 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://personal-finance-tracker-production-b165.up.railway.app'
+  apiUrl: 'https://personal-finance-tracker-production-b165.up.railway.app/api'
 };
